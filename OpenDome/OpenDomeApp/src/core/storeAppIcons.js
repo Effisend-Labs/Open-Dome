2c8505b703dbbe0d8d67c3f02e3133b96fda419b
Víctor Altamirano <v.a.i@hotmail.com>
2026-08-11 15:24:02 -0600

    commit

--- OpenDome/OpenDomeApp/src/core/storeAppIcons.js
+++ OpenDome/OpenDomeApp/src/core/storeAppIcons.js
@@ -1,9 +1,11 @@
 const demoIcon = require('../assets/store-demo.png');
 const walletIcon = require('../assets/store-wallet.png');
+const adminIcon = require('../assets/store-admin.png');
 
 export const STORE_APP_ICONS = {
   demo: demoIcon,
   wallet: walletIcon,
+  admin: adminIcon,
 };
 
 export function enrichStoreApp(app) {
